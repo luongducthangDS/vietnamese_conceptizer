f6fc7ecc1c2b981c99a0304314bfc3283c67b8cd
ldthang.dhkl16a2hn@sn.uneti.edu.vn <ldthang.dhkl16a2hn@sv.uneti.edu.vn>
2025-01-22 15:04:49 +0700

    update

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from setuptools import setup, find_packages
 setup(
     name="vietnamese_conceptizer",
     version="0.1.0",
-    author="Your Name",
-    author_email="your_email@example.com",
+    author="Bui Van Tan; Luong Duc Thang",
+    author_email="luongducthang289@gmail.com",
     description="A tool for processing text files and normalizing data using a dictionary",
     long_description=open("README.md").read(),
     long_description_content_type="text/markdown",
-    url="https://github.com/yourusername/vietnamese_conceptizer",
+    url="https://github.com/luongducthangDS/vietnamese_conceptizer",
     packages=find_packages(),
     package_data={
         "vietnamese_conceptizer": ["data/WORDS_WordNet_And_VCL_ALL_sorted.txt"]
